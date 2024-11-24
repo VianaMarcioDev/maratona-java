7b7fb8fcdcb21c36d6280a9c84cc85311dcd370a
VianaMarcioDev <marcioviana.developer@gmail.com>
2024-11-24 13:43:02 -0300

    learning about primitive types

---
+++ src/br/com/marcioviana/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package br.com.marcioviana.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        /*
+        int = inteiros 32 bits
+        double = ponto flutuante 64bits
+        float = ponto flutuante 32 bits
+        char = caracteres 2 bits
+        byte = inteiros 8 bits
+        short = inteiros = 16 bits
+        long = inteiros = 64 bits
+        boolean = true or false
+         */
+
+        char caractere = 65;
+        System.out.println(caractere);
+        System.out.println((char)caractere);
+
+        int idade = 28;
+        System.out.println((double)idade);
+
+        long age = 45L;
+        System.out.println((int)age);
+    }
+}
