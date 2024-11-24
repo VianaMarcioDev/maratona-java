e5a8ac7b504fa28c2695aaf883285d399867543c
VianaMarcioDev <marcioviana.developer@gmail.com>
2024-11-24 14:30:02 -0300

    exercise with String and concatenation

---
+++ src/br/com/marcioviana/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package br.com.marcioviana.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nomeCompleto = "Márcio de Freitas Viana";
+        String endereco = "Rua Blá blá blá, s/n";
+        double salario = 5000.0;
+        String data = "24/11/2024";
+
+        System.out.println("Eu, " + nomeCompleto + ", morando no endereço " + endereco + ", confirmo que recebi o salário de R$ " + salario + " na data " + data);
+    }
+}
