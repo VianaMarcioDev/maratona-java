b488f3a980c660b4fec29f2a6cb353cb6d20a589
VianaMarcioDev <marcioviana.developer@gmail.com>
2024-11-24 19:03:46 -0300

    Add explanations for arithmetic, logical, assignment, relational, and increment/decrement operators

---
+++ src/br/com/marcioviana/introducao/Aula04Operadores.java
@@ -0,0 +1,44 @@
+package br.com.marcioviana.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+
+        /* Aritméticos
+            + soma/concatenação
+            - subtração
+            * multiplicação
+            / divisão
+            % módulo / resto da divisão
+         */
+
+        /* Relacionais
+            < menor
+            <= menor ou igual
+            > maior
+            >= maior ou igual
+            == igualdade
+            =! diferente
+         */
+
+        /* Lógicos
+            && e    -   os dois valores tem que ser V para o resultado ser V
+            || ou   -   se apenas um resultado for V, o resultado já será V
+            ! not   -   negação
+         */
+
+        /* Atribuição
+            = atribuição
+            += soma ao valor que já havia. Ex.: x = x + 1; Isso é igual a x += 1;
+            -= subtrai do valor que já havia
+            *= multiplica do valor que já havia
+            /= divide do valor que já havia
+         */
+
+        /* Incremento / decremento
+            ++x pré-incremento de um ao x
+            x++ pós-incremento de um ao x
+            --x pré-decremento de um ao x
+            x-- pós-decremento de um ao x
+         */
+    }
+}
