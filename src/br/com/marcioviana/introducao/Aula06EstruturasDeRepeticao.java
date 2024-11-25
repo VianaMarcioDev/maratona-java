4a7eec92a503cb8a26c1ccf759c6d5dffc5388d3
VianaMarcioDev <marcioviana.developer@gmail.com>
2024-11-25 20:31:14 -0300

    Laços de repetição

---
+++ src/br/com/marcioviana/introducao/Aula06EstruturasDeRepeticao.java
@@ -0,0 +1,29 @@
+package br.com.marcioviana.introducao;
+
+public class Aula06EstruturasDeRepeticao {
+    public static void main(String[] args) {
+
+        // Laço while
+        int contador = 0;
+        while (contador < 10) {
+            System.out.println(contador);
+            contador++;
+        }
+
+        // Laço Do While
+        do{
+            System.out.println("Eita! Começou!");
+        }while(contador < 10);{
+            System.out.println(contador);
+            contador++;
+        }
+
+        // Laço for
+        for (int i = 0; i < 10; i++) {
+            System.out.println(contador);
+            contador++;
+        }
+
+
+    }
+}
