08792042b54a1497279e5b5025a4a6bbc1184718
VianaMarcioDev <marcioviana.developer@gmail.com>
2024-11-24 21:25:27 -0300

    Estrutura condiconal switch

--- src/br/com/marcioviana/introducao/Aula05EstruturasCondicionais.java
+++ src/br/com/marcioviana/introducao/Aula05EstruturasCondicionais.java
@@ -20,6 +20,34 @@ public class Aula05EstruturasCondicionais {
 
         System.out.println(canDrive);
 
+        //Switch
+
+        byte dia = 5;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+
     }
 
 }
